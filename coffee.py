811150586191d30685c4129f52c93199f9d906ef
Rick Yazwinski <ricky@thinker.ca>
2013-11-16 16:13:28 -0500

    json-ified responses

--- coffee.py
+++ coffee.py
@@ -8,13 +8,22 @@ app = bottle.Bottle()
 plugin = sqlite.Plugin(dbfile='/Users/rick/coffee/coffee.db')
 app.install(plugin)
 
+def json_return(status_code, msg):
+    import json
+    from bottle import response
+
+    response.content_type='application/json'
+    response.status = status_code
+
+    return json.dumps(msg)
+
 @app.route('/last')
 def last(db):
     row = db.execute('select dts, coffee from raw_log order by dts desc').fetchone()
     if row:
-        return "%s: %s" % (row['dts'], row['coffee'])
+        return json_return(200, "%s: %s" % (row['dts'], row['coffee']))
 
-    return "No rows"
+    return json_return(404, "No rows")
 
 def estimate_complete(dts):
     td = datetime.timedelta(seconds=300)
@@ -23,7 +32,7 @@ def estimate_complete(dts):
 @app.route('/brew/:type')
 def brew(type, db):
     if bottle.request.headers.environ.get('REMOTE_ADDR') != '127.0.0.1':
-        return "Can only start a brew from the monitor host - sorry."
+        return json_return(403, "Can only start a brew from the monitor host - sorry.")
 
     row = db.execute('select dts, coffee from raw_log order by dts desc').fetchone()
     now = datetime.datetime.utcnow()
@@ -35,12 +44,12 @@ def brew(type, db):
         if delta.seconds < 300:
             if type != str(row['coffee']):
                 db.execute('update raw_log set coffee=%s where dts="%s"' % (type, row['dts']))
-                return "brew type changed. Estimated completion: %s" % estimate_complete(dt)
+                return json_return(200, "brew type changed. Estimated completion: %s" % estimate_complete(dt))
             else:
-                return "brew already in progress. Estimated completion: %s" % estimate_complete(dt)
+                return json_return(200, "brew already in progress. Estimated completion: %s" % estimate_complete(dt))
 
     db.execute('insert into raw_log (dts, coffee) values (CURRENT_TIMESTAMP, ?)', type)
-    return "brew started. Estimated completion: %s" % estimate_complete(now)
+    return json_return(200, "brew started. Estimated completion: %s" % estimate_complete(now))
 
 @app.route('/scatter')
 def scatter(db):
@@ -51,18 +60,18 @@ def scatter(db):
         dt = datetime.datetime.strptime(row['dts'], '%Y-%m-%d %H:%M:%S')
         scatter_data[dt.hour][dt.minute/5][row['coffee']] += 1
 
-    return str(scatter_data)
+    return json_return(200, scatter_data)
 
 @app.route('/favourites')
 def favourites(db):
     data = db.execute('select coffee, count(coffee) from raw_log group by coffee;')
     ret = [(row['coffee'], row['count(coffee)']) for row in data]
-    return str(ret)
+    return json_return(200, str(ret))
 
 @app.route('/coffees')
 def coffees(db):
     data = db.execute('select id, name from coffees')
     ret = [(row['id'], row['name']) for row in data]
-    return str(ret)
+    return json_return(200, str(ret))
 
 app.run(host='localhost', port=8080, debug=True)
