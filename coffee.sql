990bc7974fb43d3172019c262f908a9203d57c7b
Rick Yazwinski <ricky@thinker.ca>
2013-11-16 08:56:35 -0500

    schema

---
+++ coffee.sql
@@ -0,0 +1,16 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE raw_log
+(
+    dts DATETIME PRIMARY KEY NOT NULL DEFAULT timestamp,
+    coffee int NOT NULL
+);
+CREATE TABLE coffees
+(
+    id int PRIMARY KEY NOT NULL,
+    name varchar(30) NOT NULL
+);
+INSERT INTO "coffees" VALUES(0,'unknown');
+INSERT INTO "coffees" VALUES(1,'house');
+INSERT INTO "coffees" VALUES(2,'sumatra');
+COMMIT;
