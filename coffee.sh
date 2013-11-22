50ad598a581bb3c622f62a0da46199c069ed839a
Rick Yazwinski <rick@tucows.com>
2013-11-22 13:12:56 -0500

    stuff to make raspberrypi autostart happy
    Signed-off-by: Rick Yazwinski <ricky@thinker.ca>

--- coffee.sh
+++ coffee.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-HOME=~
-VENVDIR=$HOME/.virtualenvs/coffee
-BINDIR=$HOME/coffee
+HOME=/home/pi
+VENVDIR=${HOME}/.virtualenvs/coffee
+BINDIR=${HOME}/coffee
 
 cd $BINDIR
-source $VENVDIR/bin/activate
-python $BINDIR/coffee.py
+. $VENVDIR/bin/activate
+python $BINDIR/coffee.py &
 echo $! > /var/run/coffee.pid
