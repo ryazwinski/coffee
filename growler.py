fbdb42c83732cad1aac1221593dd74229816c1e6
Rick Yazwinski <ricky@thinker.ca>
2013-12-19 09:59:35 -0500

    gntp needs python logging initd

--- growler.py
+++ growler.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import redis
+import logging
+logging.basicConfig(level=logging.INFO)
 import gntp.notifier
 import json
 import time
@@ -15,7 +17,7 @@ while True:
                 d_s = event.get('data')
                 data = json.loads(d_s)
                 if data.has_key('human'):
-                    gntp.notifier.mini(data['human'])
+                    gntp.notifier.mini(data['human'], callback='http://javamont.int.tucows.com:8080/')
     except redis.exceptions.ConnectionError:
         time.sleep(10)
         pass
